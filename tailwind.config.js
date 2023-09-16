87b21e301b9706b986721bab4557158f7f2ba14c
Sayed Mohabbat <sayedghazanfarmohabbat@gmail.com>
2023-09-16 09:48:02 +0430

    hi how are you

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      lg: "1024px",
+      md: "768px",
+      sm: "640px",
+      xs: "540px",
+    },
+  },
+};
